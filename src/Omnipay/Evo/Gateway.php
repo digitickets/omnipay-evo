57e44acd4b5897540c825930c4758f8601214a5d
Glenn Jenkins <glenn@digitickets.co.uk>
2015-05-20 15:42:36 +0100

    Copied files from DataCash Omnipay Driver as a starting point.

---
+++ src/Omnipay/Evo/Gateway.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Omnipay\Evo;
+
+use Omnipay\Evo\Message\CompletePurchaseRequest;
+use Omnipay\Evo\Message\PurchaseRequest;
+use Omnipay\Common\AbstractGateway;
+
+/**
+ * Evo Gateway
+ *
+ * @link https://www.evo.com/developersarea.php
+ */
+class Gateway extends AbstractGateway
+{
+    public function getName()
+    {
+        return 'Evo';
+    }
+
+    public function getDefaultParameters()
+    {
+        return array(
+            'merchantId' => '',
+            'password' => '',
+            'testMode' => false
+        );
+    }
+
+    public function getMerchantId()
+    {
+        return $this->getParameter('merchantId');
+    }
+
+    public function setMerchantId($value)
+    {
+        return $this->setParameter('merchantId', $value);
+    }
+
+    public function getPassword()
+    {
+        return $this->getParameter('password');
+    }
+
+    public function setPassword($value)
+    {
+        return $this->setParameter('password', $value);
+    }
+
+    public function purchase(array $parameters = array())
+    {
+        return $this->createRequest('\Omnipay\Evo\Message\PurchaseRequest', $parameters);
+    }
+
+    public function completePurchase(array $parameters = array())
+    {
+        return $this->createRequest('\Omnipay\Evo\Message\CompletePurchaseRequest', $parameters);
+    }
+
+    public function refund(array $parameters = array())
+    {
+        return $this->createRequest('\Omnipay\Evo\Message\RefundRequest', $parameters);
+    }
+}
