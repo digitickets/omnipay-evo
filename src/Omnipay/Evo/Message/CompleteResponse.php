d42e3c3d315679d689ba4a2ca3e2cded27d6ee57
Glenn Jenkins <glenn@digitickets.co.uk>
2015-06-03 12:23:11 +0100

    Interim commit. Added hash validation to the Complete Request response.

--- src/Omnipay/Evo/Message/CompleteResponse.php
+++ src/Omnipay/Evo/Message/CompleteResponse.php
@@ -12,26 +12,38 @@ use Omnipay\Common\Message\RequestInterface;
 class CompleteResponse extends AbstractResponse implements RedirectResponseInterface
 {
     /**
-     * @var Omnipay\Evo\Message\PurchaseRequest $request The purchase request object.
+     * @var Omnipay\Evo\Message\CompletePurchaseRequest $request The purchase request object.
      */
     protected $request;
+    
+    /**
+     * @var bool $isHashValid Says whether the hash validation worked or not.
+     */
+    protected $isHashValid;
 
     public function __construct(RequestInterface $request, $data)
     {
         // Decode the response (it's actually very simple).
         $this->data = is_array($data) ? $data : [];
         $this->request = $request;
+        $this->validateHash();
     }
 
     public function isSuccessful()
     {
-        // For now, I just test whether the errMsg has anything in it, while I wait for a
-        // definitive answer from BOIPA.
-        return (isset($this->data['Response']) && $this->data['Response'] === 'Approved');
+        // Check that the hash validation worked, and that the response was "Approved".
+        return ($this->isHashValid && isset($this->data['Response']) && $this->data['Response'] === 'Approved');
     }
     
     public function getMessage() {
-        return ($this->isSuccessful() && isset($this->data['ErrMsg']) ? $this->data['ErrMsg'] : null);
+        if ($this->isSuccessful()) {
+            return null;
+        }
+        if ($this->isHashValid) {
+            return (isset($this->data['ErrMsg']) ? $this->data['ErrMsg'] : null);
+        }
+        
+        return 'Hash validation failed';
     }
     
     public function getTransactionReference() {
@@ -48,6 +60,35 @@ class CompleteResponse extends AbstractResponse implements RedirectResponseInter
     
     public function getRedirectData() {
         return [];
+    }
+
+    /**
+     * This method is used to validate that the posted message is valid. It builds a hash
+     * value of certain values in the message. Then, the built hash is checked against
+     * the hash in the message - if different, we will raise an error. 
+     */
+    protected function validateHash() {
+        $this->isHashValid = false;
+        $localHash = '';
+        // Check that the list of values to use in the hash, and their generated hash
+        // value are present.
+        if (isset($this->data['HASHPARAMS']) && isset($this->data['HASH'])) {
+            // Loop through each parameter we've been given and append it to our hash
+            // (with no separators).
+            $params = explode(':', $this->data['HASHPARAMS']);
+            foreach ($params as $param) {
+                if (isset($this->data[$param])) {
+                    $localHash .= $this->data[$param];
+                }
+            }
+            // Then append our storeKey (which isn't included in the post), again with
+            // no separator.
+            $localHash .= $this->request->getPassword();
+            // Encode our hash value.
+            $localHash = base64_encode(sha1($localHash, true));
+            // Compare our hash with their hash.
+            $this->isHashValid = $localHash === $this->data['HASH'];
+        }
     }
 
 }
